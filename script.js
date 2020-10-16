3ef40837d7816a05c9251ba8018423d3c363ddc1
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2020-10-16 18:10:31 -0400

    update
    finished "Monthly payments total" logic/output

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 ﻿
     function calcMortgage() {
 
-
+        //List of variables (Declarations)
 
         let amountLoaned = document.getElementById("inpLoan").value;      //for example: 250K
 
@@ -11,30 +11,53 @@
 
         let numOfMonths = term * 12;                                    //Number of Monthly Payments
 
-       
-
-
-
-
-        let totalMonthlyPayment = (amountLoaned * (APR / 1200)) / (1 - (1 + (APR / 1200) ** numOfMonths));
-
-        let remainingBalance = amountLoaned;
-
-        let interestPayment = remainingBalance * (APR / 1200);
-
-        let principalPayment = totalMonthlyPayment - interestPayment;
-
-        /*At the end of each month*/ let remainingBalance = remainingBalance - principalPayment;
-
-
-
+        let base = (1 + (APR / 1200))
 
         
 
 
 
 
+        //Total Monthly Payment
+        let totalMonthlyPayment = amountLoaned * (APR / 1200) / (1 - (Math.pow(base, -numOfMonths)));
 
+        document.getElementById("output1").innerHTML = `Your Monthly Payment = $${Math.round(totalMonthlyPayment)}`;
+
+        //Remaining Balance
+        let remainingBalance = amountLoaned[i];
+
+
+        //Principal Payment
+        let principalPayment = totalMonthlyPayment - interestPayment;
+
+        document.getElementById("output2").innerHTML = `Principal Payment = $${(totalPrincipal)}`;
+
+
+        //Total principal
+        let totalInterest = amountLoaned ;
+
+        document.getElementById("output3").innerHTML = `Total Monthly Payment = $${(totalInterest)}`;
+
+
+        //Total Cost
+        let principalPayment = "";
+
+        document.getElementById("output4").innerHTML = `Total Cost of Loan = $${(principalPayment)}`;
+
+        
+
+        //Table that Indicates the Months, payments, principals, interest, total interest to date, and remaining balance
+        for (i = 0; i <= 12; i++)
+            //need to print out 
+
+
+        /*let remainingBalance = amountLoaned;
+
+        let interestPayment = remainingBalance * (APR / 1200);
+
+        let principalPayment = totalMonthlyPayment - interestPayment;
+        
+        let remainingBalance = remainingBalance - principalPayment; */
 
     };
 
