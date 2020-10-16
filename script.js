31396eaae3c53d00ab1b2fd8e3f3c243d1577c40
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2020-10-16 11:48:01 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+﻿
+function calcMortgage() {
+    let amountBorrowed = document.getElementById("inpLoan").value;         //for example: 250K
+    let downPayment = document.getElementById("inpDown").value;            //for example, 20%
+    let APR = document.getElementById("inpAPR").value;                    //percentage, for example: 5%
+    let term = document.getElementById("inpTerm").value;                   //for example: 15 or 30 years
+
+
+
+    let principal = amountBorrowed - ((downPayment * .01) * amountBorrowed);    //Amount borrowed - down payment made
+
+    let MIR = (APR * .01) / 12;         //Monthly Interest Rate
+
+    let NMP = term * 12;                //Number of Monthly Payments
+
+    let Mortgage = principal * (MIR * (1 + MIR) ** NMP) / ((1 + MIR) ** (NMP - 1));
+
+};
+
+   document.getElementById(output).innerHTML = Mortgage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
