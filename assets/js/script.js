1f19d84117268a5567ba23b1d7c050ee7f5529f2
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2020-10-18 14:43:26 -0400

    update
    Populates the table with data when the function is run

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,103 @@
 ﻿function roundUp(num) {
     return +(Math.round(num + "e+2") + "e-2");
 }
-    function calcMortgage() {
+function calcMortgage() {
+    //this variable is linking our table function to our index.html table
+    let tableOutput = document.getElementById("tableBody");
 
-        //List of variables (Declarations)
+    let amountLoaned = document.getElementById("inputAmount").value;      
 
-        let amountLoaned = document.getElementById("inputAmount").value;      //for example: 250K
+    let numOfMonths = document.getElementById("inputMonths").value;            
 
-        let numOfMonths = document.getElementById("inputMonths").value;             //for example: 15 or 30 years
+    let interestRate = document.getElementById("inputRate").value;             
 
-        let interestRate = document.getElementById("inputRate").value;             //percentage, for example: 5%
+    let base = (1 + (interestRate / 1200));
 
+    // 1) Total Monthly Payment (The payment amount)
+    let totalMonthlyPayment = amountLoaned * (interestRate / 1200) / (1 - (Math.pow(base, -numOfMonths)));
 
-        let base = (1 + (interestRate / 1200));
-
-        
-        // 1) Total Monthly Payment (The payment amount)
-        let totalMonthlyPayment = amountLoaned * (interestRate / 1200) / (1 - (Math.pow(base, -numOfMonths)));
-
-        document.getElementById("dashMonthlyPayment").innerHTML = `Your monthly payment = $${Math.round(totalMonthlyPayment)}`;
+    document.getElementById("dashMonthlyPayment").innerHTML = `Your monthly payment = $${Math.round(totalMonthlyPayment)}`;
 
 
-        // 2) Table that lists the month (1-total number of payments)
+    // 2) Table that lists the month (1-total number of payments)
 
 
-        let previousRemainingBalance = amountLoaned;
+    let previousRemainingBalance = amountLoaned;
 
-        let arrayInterest = [];
-        let arrayPrincipal = [];
-        let remainingBalance = [];
-        let totalInterest = 0;
-        let totalPrincipal = 0;
+    let arrayInterest = [];
+    let arrayPrincipal = [];
+    let remainingBalance = [];
+    let arrayTotalInterest = [];
+    let totalInterest = 0;
+    let totalPrincipal = 0;
 
-        for (let i = 0; i < numOfMonths; i++) {
-            arrayInterest.push(roundUp(previousRemainingBalance * (interestRate / 1200)));
-            arrayPrincipal.push((roundUp(totalMonthlyPayment - arrayInterest[i])));
-            remainingBalance.push((roundUp(previousRemainingBalance - arrayPrincipal[i])));
-            previousRemainingBalance = remainingBalance[i];
-            totalInterest = roundUp(totalInterest + arrayInterest[i]);
-            totalPrincipal = roundUp(totalPrincipal + arrayPrincipal[i]);
+    for (let i = 0; i < numOfMonths; i++) {
+        arrayInterest.push(roundUp(previousRemainingBalance * (interestRate / 1200)));
+        arrayPrincipal.push((roundUp(totalMonthlyPayment - arrayInterest[i])));
+        remainingBalance.push((roundUp(previousRemainingBalance - arrayPrincipal[i])));
+        previousRemainingBalance = remainingBalance[i];
+        totalInterest = roundUp(totalInterest + arrayInterest[i]);
+        arrayTotalInterest.push(totalInterest);
+        totalPrincipal = roundUp(totalPrincipal + arrayPrincipal[i]);
 
-            console.log("Principal =", arrayPrincipal[i], ", Interest =", arrayInterest[i], "Total Interest=", totalInterest, "Balance =", remainingBalance[i], "\n\n");
+        console.log(
+            "Principal =", arrayPrincipal[i],
+            ", Interest =", arrayInterest[i],
+            "Total Interest=", totalInterest,
+            "Balance =", remainingBalance[i],
+            "\n\n"
+        );
+    }
+    console.log("Total Principal =", totalPrincipal);
+    console.log("Total Interest =", roundUp(totalInterest));
+    console.log("Total Cost =", roundUp(totalPrincipal + totalInterest));
 
+    //Total Princial Output
+    document.getElementById("dashPrincipal").innerHTML = ` $${totalPrincipal}`;
 
-          
+    //Total Interest Output
+    document.getElementById("dashInterest").innerHTML = ` $${totalInterest}`;
 
-          
-        }
-        console.log("Total Principal =", roundUp(totalPrincipal));
-        console.log("Total Interest =", roundUp(totalInterest));
-        console.log("Total Cost =", roundUp(totalPrincipal + totalInterest));
-
-        //Total Princial Output
-        document.getElementById("dashPrincipal").innerHTML = ` $${totalPrincipal}`;
-
-        //Total Interest Output
-        document.getElementById("dashInterest").innerHTML = ` $${totalInterest}`;
-
-        //Total Cost Output
-        document.getElementById("dashCost").innerHTML = ` $${(totalPrincipal + totalInterest)}`;
-
-        // 3) The principal paid this month
-        //let principalPayment = totalMonthlyPayment - interestPayment;
-
-        //document.getElementById("dashPrincipal").innerHTML = `The principal paid this month = $${Math.round(principalPayment)}`;
-
-       
-
-        // 4) The Interest paid this month
-        //let interestPayment = remainingBalance * (interestRate / 1200);
-        //document.getElementById("dashInterest").innerHTML = `The interest paid this month = $${Math.round(interestPayment)}`;
+    //Total Cost Output
+    document.getElementById("dashCost").innerHTML = ` $${(totalPrincipal + totalInterest)}`;
 
 
-        // 5) The total interst paid to date
-        //let interestTotal = principalPayment * interestPayment * term;
-        //document.getElementById("").innerHTML = `The total interest paid to date = $${Math.round(interestTotal)}`;
+    //populating our table with the data
+    populateTable(numOfMonths, totalMonthlyPayment, arrayPrincipal, arrayInterest, arrayTotalInterest, remainingBalance, tableOutput);
 
-        // 6) The remaining loan balance at the end of the month
-        //let remainingbalanceBalance = amountLoaned;
-        //document.getElementById("").innerHTML = `The remaining loan balance = $${Math.round(remainingLoanBalance)}`;
-
-
-        // 7) Total Interest Paid
-        //let totalInterestPaid = ;
-
-
-
-        // 8) Total Cost
-        //let totalCost = ;
 
 };
 
+function populateTable(months, monthlyPayment, principals, interests, totalInterests, balances, table) {           //representing arrays;
+    for (let i = 0; i < months; i++) {
+        let row = table.insertRow(i);           //Insert the row into the table
+
+        let month = row.insertCell(0);          //Here is how the table will be populated
+        let payment = row.insertCell(1);
+        let principal = row.insertCell(2);
+        let interest = row.insertCell(3);
+        let totalInterest = row.insertCell(4);
+        let balance = row.insertCell(5);
+
+        month.innerHTML = i + 1;
+        payment.innerHTML = monthlyPayment;
+        principal.innerHTML = principals[i];
+        interest.innerHTML = interests[i];
+        totalInterest.innerHTML = totalInterests[i];
+        balance.innerHTML = balances[i];
+
+    }
+}
+
 function clearIt() {
-    document.getElementById("inputAmount").value="";
-    document.getElementById("inputMonths").value="";
-    document.getElementById("inputRate").value="";
+    document.getElementById("inputAmount").value = "";
+    document.getElementById("inputMonths").value = "";
+    document.getElementById("inputRate").value = "";
 }
 
 
 
-  
+
 
 document.getElementById("calcBtn").addEventListener("click", calcMortgage);
 
@@ -112,4 +110,3 @@ document.getElementById("calcBtn").addEventListener("click", calcMortgage);
 
 
 
-    
